2ae5421a9877afa3e561379c05cb4dde1bcc766b
brendabarros <85901562+brendabarros@users.noreply.github.com>
2023-02-27 18:32:20 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,40 @@
+const respositories = document.querySelector('.content-main');
+
+//Comunicação com a api do GitHub
+function getApiGitHub() {
+    fetch('https://api.github.com/users/brendabarros/repos')
+        .then(async res => {
+            if (!res.ok) {
+                throw new Error(res.status);
+            }
+
+            let data = await res.json();
+            console.log(data)
+
+            data.map(item => {
+                let project = document.createElement('div');
+                project.innerHTML = `
+                <div class="project">
+                    <div>
+                      <h4 class="title">${item.name}</h4>
+                     <span class="date-create">${Intl.DateTimeFormat('pt-BR').format(new Date(item.created_at))}</span>
+                    </div>
+                    <div class="link-img">
+                       <a href="${item.html_url} "target="_blank"><img src="img/github.png" alt=""></a>
+                       <a href="https://${item.name} "target="_blank">teste</a>
+                       
+                    </div>
+                    <div>
+                       <span class="language"><span class="circle"></span>${item.language}</span>
+                    </div>
+                </div> `
+
+                respositories.appendChild(project);
+
+            }
+            )
+
+        })
+
+}
+getApiGitHub()
